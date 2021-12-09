fc7978f0d07978eb83a6514cc2e3bf667679dc68
Vaka-vaka <Waka2006@ukr.net>
2021-12-09 14:06:17 +0200

    ц

--- README.md
+++
@@ -1,2 +0,0 @@
-# Module_6_Servlets
- 
---
+++ src/main/java/ua/goit/controller/DevelopersServlet.java
@@ -0,0 +1,33 @@
+
+
+package ua.goit.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ua.goit.model.body.Developers;
+import ua.goit.service.DevelopersService;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/developers")
+public class DevelopersServlet extends HttpServlet {
+
+    private DevelopersService service;
+
+    @Override
+    public void init() throws ServletException {
+        this.service = (DevelopersService) getServletContext().getAttribute("developersService" );
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Developers> all = service.getAll();
+        req.setAttribute("developers", all);
+        req.getRequestDispatcher("/view/jsp/developers.jsp").forward(req, resp);
+    }
+}
